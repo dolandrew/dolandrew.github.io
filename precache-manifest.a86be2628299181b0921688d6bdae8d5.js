5ef777ec5b661469e2c0c7ad51324609fe5c9471
Andrew Dolan <andrew.dolan@skyward.io>
2020-07-16 19:06:47 -0700

    Updates

---
+++ precache-manifest.a86be2628299181b0921688d6bdae8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e2b697cd01587372e05cfcd86420bd",
+    "url": "/googli-ui/index.html"
+  },
+  {
+    "revision": "e143f5381d25192ec58c",
+    "url": "/googli-ui/static/css/main.74d1aa92.chunk.css"
+  },
+  {
+    "revision": "5f897a3fd63cfe4e67c1",
+    "url": "/googli-ui/static/js/2.70fd5af3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/googli-ui/static/js/2.70fd5af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e143f5381d25192ec58c",
+    "url": "/googli-ui/static/js/main.f5f0dee2.chunk.js"
+  },
+  {
+    "revision": "028694c4352096d63ff4",
+    "url": "/googli-ui/static/js/runtime-main.2475e9fd.js"
+  },
+  {
+    "revision": "dace2d4f7d9941c9bc4230ca108ff1f9",
+    "url": "/googli-ui/static/media/phish-logo.dace2d4f.png"
+  }
+]);
